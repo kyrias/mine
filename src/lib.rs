902b4bdfe8432942f9b683791168b13c603e62fb
Johannes Löthberg <johannes@kyriasis.com>
2017-11-28 03:16:11 +0100

    reformat chain_err
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- src/lib.rs
+++ src/lib.rs
@@ -74,32 +74,24 @@ impl Repository {
 
     pub fn insert(&mut self, path: &str, content: &[u8]) -> Result<()> {
         let filename = self.mapper.insert(path);
-        fs::create_dir_all("passrep")
-            .chain_err(|| "Failed to create repository directory")?;
-        let mut file = File::create(format!("passrep/{}", filename))
-            .chain_err(|| "Failed to create file")?;
-        file.write_all(content)
-            .chain_err(|| "Failed to write content to disk")?;
+        fs::create_dir_all("passrep").chain_err(|| "Failed to create repository directory")?;
+        let mut file = File::create(format!("passrep/{}", filename)).chain_err(|| "Failed to create file")?;
+        file.write_all(content).chain_err(|| "Failed to write content to disk")?;
         Ok(())
     }
 
     pub fn get(&self, path: &str) -> Result<Vec<u8>> {
-        let filename = self.mapper.find(&path)
-            .chain_err(|| "Could not find Mapper entry")?;
-        let mut file = File::open(format!("passrep/{}", filename))
-            .chain_err(|| "Failed to open file")?;
+        let filename = self.mapper.find(&path).chain_err(|| "Could not find Mapper entry")?;
+        let mut file = File::open(format!("passrep/{}", filename)).chain_err(|| "Failed to open file")?;
         let mut buf = Vec::new();
-        file.read_to_end(&mut buf)
-            .chain_err(|| "Could not read file content")?;
+        file.read_to_end(&mut buf).chain_err(|| "Could not read file content")?;
         Ok(buf)
     }
 
     pub fn delete(&mut self, path: &str) -> Result<()> {
-        let filename = self.mapper.find(&path)
-            .chain_err(|| "Could not find Mapper entry")?;
+        let filename = self.mapper.find(&path).chain_err(|| "Could not find Mapper entry")?;
         self.mapper.remove(&path);
-        fs::remove_file(format!("passrep/{}", filename))
-            .chain_err(|| "Failed to remove file")?;
+        fs::remove_file(format!("passrep/{}", filename)).chain_err(|| "Failed to remove file")?;
         Ok(())
     }
 }
