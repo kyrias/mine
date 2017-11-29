99261d83cbcb8783874687c4b4d003ee808b4996
Johannes Löthberg <johannes@kyriasis.com>
2017-11-29 15:34:34 +0100

    Start working on repository crate docs.
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- repository/src/lib.rs
+++ repository/src/lib.rs
@@ -1,3 +1,4 @@
+//! Contains the mine filesystem helper
 #[macro_use] extern crate error_chain;
 extern crate rand;
 extern crate sequence_trie;
@@ -63,15 +64,47 @@ impl Mapper {
     }
 }
 
+/// The `Repository` struct contains the public interface for storing and retrieving files from
+/// disk.
+///
+/// It manages an internal index that is used to map virtual paths to randomly generated ASCII
+/// strings that are then used as the actual filenames.  We do this to provide more privacy than
+/// more traditional password managers like [`pass(1)`] can provide by making the on-disk filenames
+/// unrelatable to the encrypted password data stored therein.
+///
+/// Internal
+/// --------
+///
+/// Internally the index is maintained as a [`SequenceTrie`] instance.
+///
+/// `SequenceTrie` nodes can contain both a value and children at the same time, which means that
+/// in e.g. the `list()` output an entry could both contain a valid password, and children, and we
+/// don't expose those two possibility in any way.  We might want to limit this in the `insert()`
+/// function so that you can't have a node be both to prevent confusion to users.
+///
+/// TODO: Implement a `fsck` function that checks that all index entries exist on disk, and that
+/// all on-disk entries exist in the index.
+///
+/// [`SequenceTrie`]: ../sequence_trie/struct.SequenceTrie.html
+/// [`pass(1)`]: https://www.passwordstore.org/
 pub struct Repository {
     mapper: Mapper,
 }
 
 impl Repository {
+    /// Returns a new `Repository` instance.
+    ///
+    /// TODO: This should take a path to create the repository in, and open the existing index if
+    /// it already exists.
     pub fn new() -> Repository {
         Repository { mapper: Mapper::new() }
     }
 
+    /// Inserts the given path into the index and then writes the contents of the byte slice to
+    /// disk under a randomly generated filename.
+    ///
+    /// NOTE: This does not serialize the repository index to disk as well, so if we forget to do
+    /// that we'll lose track of the new file in the repository directory.
     pub fn insert(&mut self, path: &str, content: &[u8]) -> Result<()> {
         let filename = self.mapper.insert(path);
         fs::create_dir_all("passrep").chain_err(|| "Failed to create repository directory")?;
@@ -80,6 +113,7 @@ impl Repository {
         Ok(())
     }
 
+    /// Look up the given path in the index and then return the contents of the on-disk entry.
     pub fn get(&self, path: &str) -> Result<Vec<u8>> {
         let filename = self.mapper.find(&path).chain_err(|| "Could not find Mapper entry")?;
         let mut file = File::open(format!("passrep/{}", filename)).chain_err(|| "Failed to open file")?;
@@ -88,11 +122,13 @@ impl Repository {
         Ok(buf)
     }
 
+    /// List the entries available under a specific path.
     pub fn list(&self, path: &str) -> Option<Vec<String>> {
         let files = self.mapper.list(&path);
         files
     }
 
+    /// Delete an entry from the index and from disk.
     pub fn delete(&mut self, path: &str) -> Result<()> {
         let filename = self.mapper.find(&path).chain_err(|| "Could not find Mapper entry")?;
         self.mapper.remove(&path);
